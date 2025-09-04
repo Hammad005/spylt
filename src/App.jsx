e114a38377262c9043d11ad77c74da723be06652
Hammad <127682041+Hammad005@users.noreply.github.com>
2025-09-04 16:02:20 +0500

    ✨ feat(hero): implement hero section scroll animation
    - add gsap and scrolltrigger for smooth animations
    - create hero timeline for rotate, scale, and yPercent animation on scroll

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import gsap from 'gsap'
 import Navbar from './components/Navbar'
 import HeroSection from './sections/HeroSection'
+import { ScrollTrigger } from 'gsap/all'
 
+gsap.registerPlugin(ScrollTrigger);
 const App = () => {
   return (
     <main>
       <Navbar/>
       <HeroSection/>
+      <div className="h-screen"></div>
     </main>
   )
 }
